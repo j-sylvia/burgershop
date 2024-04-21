35c33046ea9c1b2ae55b4e2bb718266cf25d92d1
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-04-21 21:59:49 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,14 +6,19 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import Ratings from './Ratings';
+import { Rating } from '@mui/material'
+import Menuimg from '../images/menu-page.jpg'
+import OffCanvasExample from './Cart';
+// import Offcanvas from 'react-bootstrap/Offcanvas';
+// import '../App.css';
 
 const baseURL = "https://j-sylvia.github.io/api/db.json";
 
 function Menu() {
   const [item, setItem] = useState(null);
   const [filteritem, setFilter] = useState([]);
-
+  const [cart, setCart] = useState([]);
+  
   useEffect(() => {
     axios.get(baseURL)
       .then(res => {
@@ -27,46 +32,70 @@ function Menu() {
     setFilter(item.filter(f => f.name.toLowerCase().includes(event.target.value.toLowerCase())));
   }
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+
+  }
+
+  const shuffledData = shuffleArray(filteritem);
+
+const addToCart = (product) => {
+    setCart(prevCart=>[...prevCart, product]);
+    console.log("Adding to cart:", product); 
+    console.log("Adding to cart:", cart);
+    alert("Item added to cart");
+  }
 
   if (!item) return null;
 
   return (
     <>
-    <Image src="https://jthemes.net/themes/html/testo/files/images/menu-page.jpg" style={{marginBottom:'30px',height:'400px',width:'100%'}} fluid />
+    <div style={{ position: 'relative', textAlign: 'center', color: 'white' }}>
+      <Image
+        src={Menuimg}
+        style={{marginBottom: '30px',height: '360px',width: '100%',opacity: '0.6'}} fluid />
+      <h1 style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',fontFamily:'sans-serif',fontWeight:'bold',fontSize:'3.2rem' }}>
+        OUR MENU
+      </h1>
+    </div>
+  
     <div className='container'>
       <Row xs={1} sm={2} md={3} lg={4} className="g-4">
         <input type="text" className='form-control' name='name' placeholder='Search here...' onChange={Filter} />
-        {filteritem.map((item) => (
-          <Col key={item.id}>
-            <Card style={{width: '19rem',padding:'10px'}}>
-              <Card.Img variant="top" src={item.imgdata} style={{height:'230px',marginBottom:'10px'}}/>
-              {/* <b style={{fontSize:'larger',color:'green'}}>₹: {item.price}</b> */}
-              <div className="price-rating d-flex justify-content-between">
-                 Price : ₹ {item.price}
-                 <Ratings value={item.rating} />
-                 
-                </div>
+        {shuffledData.map((product) => (
+          <Col key={product.id}>
+            <Card style={{width:'19rem', padding:'10px'}} className="card-hover">
 
+              <Card.Img variant="top" src={product.imgdata} style={{height:'230px',marginBottom:'10px'}}/>
               
-              {/* <b>Restaurant: {item.rname}</b> */}
+              <div className="price-rating d-flex justify-content-between">
+                 Price : ₹ {product.price}
+                 <Rating value={Number(product.rating)} />
+                 </div>
+           
               <Card.Body>
-                <Card.Title><b>{item.name}</b> 
-                {/* <small style={{ fontSize: 15, color: "grey" }}>{item.rname}</small> */}
+                <Card.Title><b>{product.name}</b> 
                 </Card.Title>
                 <Card.Text>
                 
-                ☞ <small>{item.overview}</small>
+                ☞ <small>{product.overview}</small>
                   
                 </Card.Text>
-                <Button style={{ marginBottom: '10px' }} variant="success">Add to cart</Button><br />
+                <Button style={{marginBottom:'10px'}} onClick={() => addToCart(product)} variant="success">Add to cart</Button><br />
                 
               </Card.Body>
-              <small style={{color:'#807c7a'}}>{item.orders}</small>
+              <small style={{color:'#807c7a'}}>{product.orders}</small>
             </Card>
           </Col>
         ))}
 
       </Row>
+      <OffCanvasExample cart={cart} addToCart={addToCart} />
+    
     </div>
     </>
   );
