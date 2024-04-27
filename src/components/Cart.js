f6fc7ea9cce162281e23a25f02aed4dfe680a8d9
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-04-27 17:18:36 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,29 +4,41 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import React from 'react';
 // import {addToCart} from './Menu'
 
-function OffCanvasExample({ cart, addToCart, ...props }) {
+function OffCanvasExample({ cart, addToCart,removeFromCart, ...props }) {
   const [show, setShow] = useState(false);
-
+  
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const calculateTotal = () => {
+    return cart.reduce((total, currentItem) => {
+      const itemPrice = currentItem.price || 0;
+      return total + itemPrice;
+    }, 0);
+  }
+  const totalPrice = calculateTotal();
+
   return (
     <>
-      <Button variant="dark" onClick={handleShow} className="me-2">
-        <i className='fas fa-cart-plus'></i>
+      <Button variant="dark" style={{margin:'12px auto',width:'118px'}} onClick={handleShow} className="me-2 fixed-top">
+        <i className='fas fa-cart-plus'> View Cart</i>
       </Button>
       <Offcanvas show={show} onHide={handleClose} {...props}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Cart Items</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
         {cart && cart.map(product => (
-            <div key={product.id}>
-              <h4>{product.name}</h4>
-              <p>{product.rname}</p>
-              {/* <Button onClick={() => addToCart(product)}>Add to cart</Button> */}
+            <div style={{backgroundColor:'#40a8a8',padding:'20px',marginBottom:'10px',border:'1px solid black'}} key={product.id}>
+            <img src={product.imgdata} alt={'images'} style={{float:'right',height:'80px',width:'100px'}}/>
+              <h4>{product.name}</h4> 
+              <b>Restaurant: {product.rname}</b>
+              <h6>Ratings: {product.rating}</h6>
+              <h5>Price : ₹ {product.price}</h5>
+              <Button variant='dark' onClick={() => removeFromCart(product.id)} style={{float:'right',marginTop:'-25px'}}>Remove</Button>
             </div>
           ))}
+          <h3 style={{color:'darkgreen',textAlign:'center'}}>Total Price: {totalPrice}</h3>
         </Offcanvas.Body>
       </Offcanvas>
     </>
